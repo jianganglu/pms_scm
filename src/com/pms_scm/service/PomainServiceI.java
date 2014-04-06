8902f58bbc4e7ce91757318b17ea63a3dfc61960
Administrator <Administrator@PC-20120429ADAP>
2014-04-06 09:33:27 +0800

    pomain

---
+++ src/com/pms_scm/service/PomainServiceI.java
@@ -0,0 +1,14 @@
+package com.pms_scm.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jeecgframework.core.common.model.json.DataGrid;
+import org.jeecgframework.core.common.service.CommonService;
+
+import com.pms_scm.entity.Pomain;
+
+public interface PomainServiceI extends CommonService{
+
+	public void dataGrid(Pomain pomain, HttpServletRequest reqeust, HttpServletResponse response, DataGrid datagrid);
+}
---
+++ src/com/pms_scm/service/impl/PomainServiceImpl.java
@@ -0,0 +1,40 @@
+package com.pms_scm.service.impl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jeecg.system.service.SystemService;
+
+import org.jeecgframework.core.common.hibernate.qbc.CriteriaQuery;
+import org.jeecgframework.core.common.model.json.DataGrid;
+import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
+import org.jeecgframework.tag.core.easyui.TagUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pms_scm.entity.Pomain;
+import com.pms_scm.service.PomainServiceI;
+
+@Service("pomainService")
+@Transactional
+public class PomainServiceImpl extends CommonServiceImpl implements PomainServiceI{
+
+	private SystemService systemService;
+
+	@Autowired
+	public void setSystemService(SystemService systemService) {
+		this.systemService = systemService;
+	}
+
+	@Override
+	public void dataGrid(Pomain pomain, HttpServletRequest reqeust,
+			HttpServletResponse response, DataGrid dataGrid) {
+		CriteriaQuery cq = new CriteriaQuery(Pomain.class, dataGrid);
+		org.jeecgframework.core.extend.hqlsearch.HqlGenerateUtil.installHql(cq,
+				pomain);
+		systemService.getDataGridReturn(cq, true);
+		TagUtil.datagrid(response, dataGrid);
+	}
+	
+}
